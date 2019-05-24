3798eb77aecb27091c8dbfd589e576655df48846
juuuunho <50973181+juuuunho@users.noreply.github.com>
2019-05-24 10:48:39 +0900

    Add files via upload

---
+++ A023_Susi3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A023_Susi3
+{
+    class Program
+    {
+    //    //문제: 주사위 두개를 던져서 나오는 숫자의 합을 구한다
+    //    // 100만번 던진다
+    //    //출력: 
+    //    //2가나오는 횟수는 몇번
+    //    //
+        static void Main(string[] args)
+        {
+    //        Random r = new Random();
+    //        int[]n1 = new int [1000000];
+    //        int [] n2 = new int [1000000];
+    //        int[] cnt = new int[11];
+    //        int[] sum = new int[1000000];
+    //        int j = 2;
+    //        for (int i = 0; i<1000000; i++)
+    //        {
+    //            n1[i] = r.Next(1,7); ;
+    //            n2[i] = r.Next(1, 7);
+    //            sum[i] = n1[i] + n2[i];
+    //            if (sum[i] == 2)
+    //                cnt[0]++;
+    //            else if (sum[i] == 3)
+    //                cnt[1]++;
+    //            else if (sum[i] == 4)
+    //                cnt[2]++;
+    //            else if (sum[i] == 5)
+    //                cnt[3]++;
+    //            else if (sum[i] == 6)
+    //                cnt[4]++;
+    //            else if (sum[i] == 7)
+    //                cnt[5]++;
+    //            else if (sum[i] == 8)
+    //                cnt[6]++;
+    //            else if (sum[i] == 9)
+    //                cnt[7]++;
+    //            else if (sum[i] == 10)
+    //                cnt[8]++;
+    //            else if (sum[i] == 11)
+    //                cnt[9]++;
+    //            else if (sum[i] == 12)
+    //                cnt[10]++;
+    //        }
+            
+    //        foreach (var item in cnt)
+    //        {                
+    //            Console.Write("{0} 나온횟수", j);
+    //                Console.Write(item + " ");
+    //                Console.WriteLine();
+    //            j++;
+    //        }  
+        }
+    }
+}
